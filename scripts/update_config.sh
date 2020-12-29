92139e15749162a4df6908a0c94c57ba000770a3
caldric <cvaldric@gmail.com>
2020-12-29 05:35:38 -0500

    Update git commit message on shell script

--- scripts/update_config.sh
+++ scripts/update_config.sh
@@ -4,14 +4,14 @@ settings_json() {
     git add .
     git commit -S -m "Update settings.json"
     git push
-    echo "Updated settings.json"
+    echo "Updated settings.json using shell script"
 }
 
 zshrc() {
     cd ~/projects/config 
     cp ~/.zshrc . 
     git add . 
-    git commit -S -m "Update .zshrc" 
+    git commit -S -m "Update .zshrc using shell script" 
     git push
     echo "Updated .zshrc"
 }
