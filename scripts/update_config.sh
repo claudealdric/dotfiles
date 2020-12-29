5ee8af92cb87a060ac66379880f2b0ac197aa87e
caldric <cvaldric@gmail.com>
2020-12-29 05:29:28 -0500

    Add shell script

---
+++ scripts/update_config.sh
@@ -0,0 +1,20 @@
+bashrc() {
+    cd ~/projects/config 
+    cp ~/.config/Code/User/settings.json .
+    git add .
+    git commit -S -m "Update settings.json"
+    git push
+    echo "Updated settings.json"
+}
+
+zshrc() {
+    cd ~/projects/config 
+    cp ~/.zshrc . 
+    git add . 
+    git commit -S -m "Update .zshrc" 
+    git push
+    echo "Updated .zshrc"
+}
+
+"$@"
+
