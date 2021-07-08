b8f4cb5c87e6efadea87d78d7d912fc6773bbb59
claudealdric <cvaldric@gmail.com>
2021-07-07 22:53:01 -0400

    Reorder shell commands for Linux setup script

--- scripts/fish-setup.fish
+++ scripts/fish-setup.fish
@@ -1,8 +1,39 @@
 #!usr/bin/fish
 
+# Upgrade Git to the latest version
+sudo add-apt-repository ppa:git-core/ppa
+
+sudo apt update
+
+sudo apt install git -y
+
+# Add Homebrew to PATH
+set -U fish_user_paths /home/linuxbrew/.linuxbrew/bin $PATH
+
+# Install Homebrew dependencies
+sudo apt install build-essential -y
+
+# Install Homebrew-recommended software
+brew install gcc
+
+# Install VS Code
+sudo snap install code --classic
+
+# Install Discord
+sudo snap install discord
+
+# Install Vim
+sudo apt install vim -y
+
 # Set default editor to Vim
 set --global EDITOR /usr/bin/vim
 git config --global core.editor "vim"
+
+# Install dconf-editor (for editing the dock)
+sudo apt install dconf-editor -y
+
+# Install gnome-shell-extension-prefs (for hiding desktop icons)
+sudo apt install gnome-shell-extension-prefs
 
 # Set up global git configs
 git config --global user.name "claudealdric"
@@ -28,15 +59,6 @@ set --universal nvm_default_version lts
 # Install Yarn
 nvm use lts
 npm i -g yarn
-
-# Add Homebrew to PATH
-set -U fish_user_paths /home/linuxbrew/.linuxbrew/bin $PATH
-
-# Install Homebrew dependencies
-sudo apt install build-essential -y
-
-# Install Homebrew-recommended software
-brew install gcc
 
 # Add abbreviations
 abbr --add c clear
