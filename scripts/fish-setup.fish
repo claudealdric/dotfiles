ccdde1d0bdf827ae240caecd5b90116a7c6822f0
claudealdric <cvaldric@gmail.com>
2021-07-07 23:39:51 -0400

    Remove retina version of FiraCode NF

--- scripts/fish-setup.fish
+++ scripts/fish-setup.fish
@@ -68,7 +68,6 @@ curl -fLo "Fira Code Regular Nerd Font Complete.ttf" https://github.com/ryanoasi
 curl -fLo "Fira Code Bold Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Bold/complete/Fira%20Code%20Bold%20Nerd%20Font%20Complete.ttf
 curl -fLo "Fira Code Light Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Light/complete/Fira%20Code%20Light%20Nerd%20Font%20Complete.ttf
 curl -fLo "Fira Code Medium Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Medium/complete/Fira%20Code%20Medium%20Nerd%20Font%20Complete.ttf
-curl -fLo "Fira Code Retina Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Retina/complete/Fira%20Code%20Retina%20Nerd%20Font%20Complete.ttf
 cd ~
 
 # Add abbreviations
