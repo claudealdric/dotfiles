86e38a9b32be8dc3a91029ce53c7998701f6ca2a
claudealdric <cvaldric@gmail.com>
2021-07-08 00:35:19 -0400

    Add fish configuration file

---
+++ config.fish
@@ -0,0 +1,2 @@
+set fish_function_path /home/claude/.config/fish/functions /etc/fish/functions /usr/share/ubuntu/fish/vendor_functions.d /usr/local/share/fish/vendor_functions.d /usr/share/fish/vendor_functions.d /var/lib/snapd/desktop/fish/vendor_functions.d /usr/share/fish/functions "/usr/share/powerline/bindings/fish"
+powerline-setup
