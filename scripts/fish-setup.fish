a520d24646be7b4a5dbd9ae867ecfb56e5113495
claudealdric <cvaldric@gmail.com>
2021-07-07 23:34:38 -0400

    Add Fira Code Nerd Font installation

--- scripts/fish-setup.fish
+++ scripts/fish-setup.fish
@@ -2,9 +2,7 @@
 
 # Upgrade Git to the latest version
 sudo add-apt-repository ppa:git-core/ppa
-
 sudo apt update
-
 sudo apt install git -y
 
 # Add Homebrew to PATH
@@ -60,8 +58,17 @@ set --universal nvm_default_version lts
 nvm use lts
 npm i -g yarn
 
+# Install Fira Code Nerd Font
+mkdir -p ~/.local/share/fonts
+cd ~/.local/share/fonts/
+curl -fLo "Fira Code Regular Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Regular/complete/Fira%20Code%20Regular%20Nerd%20Font%20Complete.ttf
+curl -fLo "Fira Code Bold Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Bold/complete/Fira%20Code%20Bold%20Nerd%20Font%20Complete.ttf
+curl -fLo "Fira Code Light Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Light/complete/Fira%20Code%20Light%20Nerd%20Font%20Complete.ttf
+curl -fLo "Fira Code Medium Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Medium/complete/Fira%20Code%20Medium%20Nerd%20Font%20Complete.ttf
+curl -fLo "Fira Code Retina Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/raw/master/patched-fonts/FiraCode/Retina/complete/Fira%20Code%20Retina%20Nerd%20Font%20Complete.ttf
+cd ~
+
 # Add abbreviations
 abbr --add c clear
 abbr --add gac "git add --all && git commit -S"
-
 
