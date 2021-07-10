72ff1bbdb776220ce8f56fe5128b85e9bb1f88b5
claudealdric <cvaldric@gmail.com>
2021-07-10 04:24:37 -0400

    Add conditionals to check for installation status

--- scripts/bash-setup.sh
+++ scripts/bash-setup.sh
@@ -4,15 +4,30 @@
 sudo apt update && sudo apt upgrade -y
 
 # Install curl
-sudo apt install curl -y
+if ! command -v curl &> /dev/null
+then
+  sudo apt install curl -y
+else
+  echo "curl already installed"
+fi
 
 # Install wget
-sudo apt install wget -y
+if ! command -v wget &> /dev/null
+then
+  sudo apt install wget -y
+else
+  echo "wget already installed"
+fi
 
-# Install latest version of fish
-sudo add-apt-repository ppa:fish-shell/release-3
-sudo apt update
-sudo apt install fish -y
+# Install fish
+if ! command -v fish &> /dev/null
+then
+  sudo apt install fish -y
+else
+  sudo add-apt-repository ppa:fish-shell/release-3
+  sudo apt update
+  sudo apt install fish -y
+fi
 
 # Change default shell to fish
 chsh -s `which fish`
