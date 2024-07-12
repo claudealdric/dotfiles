fab575decd390ac67a2e20705c0e0147bdeda9e8
claudealdric <cvaldric@gmail.com>
2024-07-12 04:13:57 -0400

    Improve the performance of telescope via fzf
    It is suggested to install one native sorter, for better performance.
    For more information, please refer to the link below:
    
    https://github.com/nvim-telescope/telescope.nvim/wiki/Extensions

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,5 +1,12 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.8",
-	dependencies = { "nvim-lua/plenary.nvim" },
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-telescope/telescope-fzf-native.nvim", -- to improve the performance of telescope
+		build = "make",
+	},
+	config = function()
+		require("telescope").load_extension("fzf")
+	end,
 }
