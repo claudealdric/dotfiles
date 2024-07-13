ca2dbe6a5bd1b237e64c09f29e11f58279a595aa
claudealdric <cvaldric@gmail.com>
2024-07-13 12:00:18 -0400

    Disable the which-key plugin on operators
    When pressing `d$`, there would be a significant delay before `$` is
    registered.

--- .config/nvim/lua/claudealdric/plugins/which-key.lua
+++ .config/nvim/lua/claudealdric/plugins/which-key.lua
@@ -1,10 +1,11 @@
 return {
 	"folke/which-key.nvim",
+	enabled = true,
 	event = "VeryLazy",
 	opts = {
-		-- your configuration comes here
-		-- or leave it empty to use the default settings
-		-- refer to the configuration section below
+		plugins = {
+			operators = false,
+		},
 	},
 	keys = {
 		{
