ad39fb7f65a42173e4b434475cb0d847ce2c240b
claudealdric <cvaldric@gmail.com>
2024-07-12 23:26:20 -0400

    Add the treesitter plugin to nvim

---
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -0,0 +1,43 @@
+return {
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+
+		configs.setup({
+			auto_install = true,
+			ensure_installed = {
+				"bash",
+				"css",
+				"dockerfile",
+				"git_config",
+				"git_rebase",
+				"gitattributes",
+				"gitcommit",
+				"gitignore",
+				"go",
+				"gomod",
+				"gosum",
+				"graphql",
+				"html",
+				"javascript",
+				"json",
+				"lua",
+				"sql",
+				"toml",
+				"typescript",
+				"vim",
+				"yaml",
+			},
+			ignore_install = {
+				"tmux",
+			},
+			highlight = {
+				enable = true,
+				disable = {
+					"tmux",
+				},
+			},
+		})
+	end,
+}
