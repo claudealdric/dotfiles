987c0d4801a911de9dbbdfa619ccb75ae8f7f11b
claudealdric <cvaldric@gmail.com>
2024-07-14 01:03:14 -0400

    Auto-format file using stylua

--- .config/nvim/lua/claudealdric/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/claudealdric/plugins/nvim-lspconfig.lua
@@ -6,11 +6,12 @@ return {
 		lspconfig.gopls.setup({
 			on_attach = function()
 				-- vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<cr>", opts)
-				vim.keymap.set("n", "K", vim.lsp.buf.hover , opts)
-				vim.keymap.set("n", "gd", vim.lsp.buf.definition , opts)
-				vim.keymap.set("n", "gi", vim.lsp.buf.implementation , opts)
-				vim.keymap.set("n", "<F2>", vim.lsp.buf.rename , opts)
-				vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action , opts)
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+				vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+				vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, opts)
+				vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, opts)
 			end,
 		})
 	end,
