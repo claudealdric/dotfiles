507e52b89d74f0109f23c957c4ee8ef01d59dfd4
claudealdric <cvaldric@gmail.com>
2024-07-13 23:57:49 -0400

    Add additional keymaps for LSP actions

--- .config/nvim/lua/claudealdric/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/claudealdric/plugins/nvim-lspconfig.lua
@@ -7,6 +7,10 @@ return {
 			on_attach = function()
 				-- vim.keymap.set("n", "K", "<cmd>Lspsaga hover_doc<cr>", opts)
 				vim.keymap.set("n", "K", vim.lsp.buf.hover , opts)
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition , opts)
+				vim.keymap.set("n", "gi", vim.lsp.buf.implementation , opts)
+				vim.keymap.set("n", "<F2>", vim.lsp.buf.rename , opts)
+				vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action , opts)
 			end,
 		})
 	end,
