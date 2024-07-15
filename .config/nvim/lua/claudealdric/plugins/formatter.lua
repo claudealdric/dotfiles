7e2c896908dcd0a97572cbeadb8a907249cd8629
claudealdric <cvaldric@gmail.com>
2024-07-14 23:38:10 -0400

    Rework formatter configuration

--- .config/nvim/lua/claudealdric/plugins/formatter.lua
+++ .config/nvim/lua/claudealdric/plugins/formatter.lua
@@ -1,36 +1,37 @@
 return {
 	"stevearc/conform.nvim",
 	event = { "BufReadPre", "BufNewFile" },
-	opts = {
-		formatters_by_ft = {
-			css = { { { "prettierd", "prettier" } } },
-			go = { "gofmt" },
-			graphql = { { { "prettierd", "prettier" } } },
-			html = { { { "prettierd", "prettier" } } },
-			javascript = { { { "prettierd", "prettier" } } },
-			javascriptreact = { { { "prettierd", "prettier" } } },
-			json = { { { "prettierd", "prettier" } } },
-			lua = { "stylua" },
-			markdown = { { { "prettierd", "prettier" } } },
-			typescript = { { { "prettierd", "prettier" } } },
-			typescriptreact = { { { "prettierd", "prettier" } } },
-			yaml = { { { "prettierd", "prettier" } } },
-			-- Use the "_" filetype to run formatters on filetypes that don't
-			-- have other formatters configured.
-			["_"] = { "trim_whitespace" },
-		},
-		format_on_save = {
-			-- These options will be passed to conform.format()
-			timeout_ms = 500,
-			lsp_format = "fallback",
-		},
-	},
+	opts = {},
 	config = function()
-		vim.keymap.set({ "n", "v" }, "<leader>;", function()
-			require("conform").format({
-				timeout_ms = 1000,
+		local conform = require("conform")
+
+		conform.setup({
+			formatters_by_ft = {
+				css = { { { "prettierd", "prettier" } } },
+				go = { "gofmt" },
+				graphql = { { { "prettierd", "prettier" } } },
+				html = { { { "prettierd", "prettier" } } },
+				javascript = { { { "prettierd", "prettier" } } },
+				javascriptreact = { { { "prettierd", "prettier" } } },
+				json = { { { "prettierd", "prettier" } } },
+				lua = { "stylua" },
+				markdown = { { { "prettierd", "prettier" } } },
+				typescript = { { { "prettierd", "prettier" } } },
+				typescriptreact = { { { "prettierd", "prettier" } } },
+				yaml = { { { "prettierd", "prettier" } } },
+				-- Use the "_" filetype to run formatters on filetypes that don't
+				-- have other formatters configured.
+				["_"] = { "trim_whitespace" },
+			},
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
 				lsp_format = "fallback",
-			})
+			},
+		})
+
+		vim.keymap.set({ "n", "v" }, "<leader>;", function()
+			require("conform").format()
 		end, { desc = "Format file (or range in visual mode)" })
 	end,
 }
