9a55d3539373deb6cea6ae30b8a2f9613e809da3
claudealdric <cvaldric@gmail.com>
2024-07-14 23:05:20 -0400

    Add a tool installer plugin for nvim

--- .config/nvim/lua/claudealdric/plugins/lsp/mason.lua
+++ .config/nvim/lua/claudealdric/plugins/lsp/mason.lua
@@ -2,10 +2,12 @@ return {
 	"williamboman/mason.nvim",
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
 	config = function()
 		local mason = require("mason")
 		local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
 		mason.setup({
 			-- ui = {
@@ -38,6 +40,16 @@ return {
 				"yamlls",
 			},
 			automatic_installation = true,
+		})
+
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"stylua", -- lua formatter
+				"prettierd", -- prettier as a daemon
+				"prettier", -- prettier formatter
+				"codespell",
+			},
+			auto_update = true,
 		})
 	end,
 }
