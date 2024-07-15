c362e103711429e4fbc3934912a8f22ebd26861d
claudealdric <cvaldric@gmail.com>
2024-07-15 16:13:48 -0400

    Enable telescope file and text search in hidden files and directories

--- .config/nvim/lua/claudealdric/plugins/telescope.lua
+++ .config/nvim/lua/claudealdric/plugins/telescope.lua
@@ -8,7 +8,37 @@ return {
 		"folke/todo-comments.nvim",
 	},
 	config = function()
-		require("telescope").load_extension("fzf")
+		local telescope = require("telescope")
+
+		local ignore_git_blob = "!**/.git/*"
+		local vimgrep_arguments =
+			{ unpack(require("telescope.config").values.vimgrep_arguments) }
+		-- I want to search in hidden/dot files.
+		table.insert(vimgrep_arguments, "--hidden")
+		-- I don't want to search in the `.git` directory.
+		table.insert(vimgrep_arguments, "--glob")
+		table.insert(vimgrep_arguments, ignore_git_blob)
+
+		telescope.setup({
+			defaults = {
+				-- `hidden = true` is not supported in text grep commands.
+				vimgrep_arguments = vimgrep_arguments,
+			},
+			pickers = {
+				find_files = {
+					-- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+					find_command = {
+						"rg",
+						"--files",
+						"--hidden",
+						"--glob",
+						ignore_git_blob,
+					},
+				},
+			},
+		})
+
+		telescope.load_extension("fzf")
 
 		-- Helpful mappings: https://github.com/nvim-telescope/telescope.nvim?tab=readme-ov-file#default-mappings
 		local keymap = vim.keymap
