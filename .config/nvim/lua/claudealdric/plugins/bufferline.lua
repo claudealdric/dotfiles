188182e68ffaa25ceebc2488d432d7bf5ad4c13a
claudealdric <cvaldric@gmail.com>
2024-07-14 20:39:36 -0400

    Add the bufferline plugin for nvim
    This prettifies the nvim tabs.

---
+++ .config/nvim/lua/claudealdric/plugins/bufferline.lua
@@ -0,0 +1,13 @@
+return {
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		options = {
+			mode = "tabs",
+			separator_style = "slant",
+			auto_toggle_bufferline = false,
+			sort_by = "tabs",
+		},
+	},
+}
