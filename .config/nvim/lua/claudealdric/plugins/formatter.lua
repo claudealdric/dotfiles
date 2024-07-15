4ff7f0b0324333d872a02effc9086c3625023b7c
claudealdric <cvaldric@gmail.com>
2024-07-14 23:05:07 -0400

    Add a formatter plugin for nvim

---
+++ .config/nvim/lua/claudealdric/plugins/formatter.lua
@@ -0,0 +1,38 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		formatters_by_ft = {
+			css = { { { "prettierd", "prettier" } } },
+			go = { "gofmt" },
+			graphql = { { { "prettierd", "prettier" } } },
+			html = { { { "prettierd", "prettier" } } },
+			javascript = { { { "prettierd", "prettier" } } },
+			javascriptreact = { { { "prettierd", "prettier" } } },
+			json = { { { "prettierd", "prettier" } } },
+			lua = { "stylua" },
+			markdown = { { { "prettierd", "prettier" } } },
+			typescript = { { { "prettierd", "prettier" } } },
+			typescriptreact = { { { "prettierd", "prettier" } } },
+			yaml = { { { "prettierd", "prettier" } } },
+			-- Use the "*" filetype to run formatters on all filetypes.
+			["*"] = { "codespell" },
+			-- Use the "_" filetype to run formatters on filetypes that don't
+			-- have other formatters configured.
+			["_"] = { "trim_whitespace" },
+		},
+		format_on_save = {
+			-- These options will be passed to conform.format()
+			timeout_ms = 500,
+			lsp_format = "fallback",
+		},
+	},
+	config = function()
+		vim.keymap.set({ "n", "v" }, "<leader>;", function()
+			require("conform").format({
+				timeout_ms = 1000,
+				lsp_format = "fallback",
+			})
+		end, { desc = "Format file (or range in visual mode)" })
+	end,
+}
