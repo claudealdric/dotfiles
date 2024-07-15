575aa6597d586d9e4c09dcdbf4436863c3a46dbc
claudealdric <cvaldric@gmail.com>
2024-07-14 21:16:13 -0400

    Enable treesitter integration with nvim-autopairs

--- .config/nvim/lua/claudealdric/plugins/nvim-autopairs.lua
+++ .config/nvim/lua/claudealdric/plugins/nvim-autopairs.lua
@@ -1,7 +1,11 @@
 return {
 	"windwp/nvim-autopairs",
 	event = "InsertEnter",
-	config = true,
-	-- use opts = {} for passing setup options
-	-- this is equalent to setup({}) function
+	opts = {
+		check_ts = true,
+		ts_config = {
+			lua = { "string" }, -- it will not add a pair on that treesitter node
+			javascript = { "template_string" },
+		},
+	},
 }
