35883a6cc814eb1695e71c7d0a93016b35ca74dc
claudealdric <cvaldric@gmail.com>
2024-07-14 23:27:29 -0400

    Remove the mason-tool-installer plugin from nvim

--- .config/nvim/lua/claudealdric/plugins/lsp/mason.lua
+++ .config/nvim/lua/claudealdric/plugins/lsp/mason.lua
@@ -2,12 +2,10 @@ return {
 	"williamboman/mason.nvim",
 	dependencies = {
 		"williamboman/mason-lspconfig.nvim",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
 	},
 	config = function()
 		local mason = require("mason")
 		local mason_lspconfig = require("mason-lspconfig")
-		local mason_tool_installer = require("mason-tool-installer")
 
 		mason.setup({
 			-- ui = {
@@ -40,16 +38,6 @@ return {
 				"yamlls",
 			},
 			automatic_installation = true,
-		})
-
-		mason_tool_installer.setup({
-			ensure_installed = {
-				"stylua", -- lua formatter
-				"prettierd", -- prettier as a daemon
-				"prettier", -- prettier formatter
-				"codespell",
-			},
-			auto_update = true,
 		})
 	end,
 }
