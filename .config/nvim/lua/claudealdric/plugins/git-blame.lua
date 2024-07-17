aeb98f52905c763fbcfdb4ddcda7cdef0632abf9
claudealdric <cvaldric@gmail.com>
2024-07-17 13:46:26 -0400

    Add a potential (currently disabled) git blame plugin to the plugins list

---
+++ .config/nvim/lua/claudealdric/plugins/git-blame.lua
@@ -0,0 +1,17 @@
+-- NOTE: this is a cool plugin for git blame, but I currently don't have a need
+-- for git blames at the moment of writing.
+--
+-- Here are some cool commands of note:
+--
+-- - Copy SHA hash
+-- - Copy commit URL
+-- - Open file URL in browser
+-- - Copy file URL
+--
+-- For a full list of commands, check out thi link below:
+-- https://github.com/f-person/git-blame.nvim?tab=readme-ov-file#commands
+--
+return {
+	"f-person/git-blame.nvim",
+	enabled = false,
+}
