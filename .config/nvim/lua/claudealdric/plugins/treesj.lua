fe45cd4f84fce9c4f76da11d53ff924ae571d4f1
claudealdric <cvaldric@gmail.com>
2024-07-17 13:34:11 -0400

    Add the TreeSJ plugin to split and join blocks of code (e.g., array)

---
+++ .config/nvim/lua/claudealdric/plugins/treesj.lua
@@ -0,0 +1,21 @@
+return {
+	"Wansmer/treesj",
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	keys = {
+		{
+			"<leader>ts",
+			"<cmd>TSJSplit<cr>",
+			desc = "TreeSJ split blocks",
+			noremap = true,
+		},
+		{
+			"<leader>tj",
+			"<cmd>TSJJoin<cr>",
+			desc = "TreeSJ join blocks",
+			noremap = true,
+		},
+	},
+	opts = {
+		use_default_keymaps = false,
+	},
+}
