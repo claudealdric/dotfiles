92a17a03d03aa9f8dd65ba0d0383660e0dd09021
claudealdric <cvaldric@gmail.com>
2024-07-17 23:37:47 -0400

    Turn on spellchecker for markdown, text, and git commits

--- .config/nvim/lua/claudealdric/core/autocommands.lua
+++ .config/nvim/lua/claudealdric/core/autocommands.lua
@@ -33,3 +33,11 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 		vim.lsp.buf.format({ async = false })
 	end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "markdown", "text", "gitcommit" },
+	callback = function()
+		vim.opt_local.spell = true
+		vim.opt_local.spelllang = "en_us"
+	end,
+})
