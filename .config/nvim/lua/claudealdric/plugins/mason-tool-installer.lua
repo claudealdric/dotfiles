000330254438e503122e32ca4b83d647320dc2d1
claudealdric <cvaldric@gmail.com>
2024-07-23 23:21:06 -0400

    Add the mason tool installer
    On a new machine, I would have to manually install the required tools
    via the Mason UI.

---
+++ .config/nvim/lua/claudealdric/plugins/mason-tool-installer.lua
@@ -0,0 +1,19 @@
+return {
+	"WhoIsSethDaniel/mason-tool-installer.nvim",
+	dependencies = {
+		"williamboman/mason.nvim",
+	},
+	event = "VeryLazy",
+	opts = {
+		ensure_installed = {
+			"delve", -- DAP for Go
+			"eslint_d",
+			"golangci-lint",
+			"prettierd",
+		},
+		run_on_start = true,
+	},
+	keys = {
+		{ "<leader>ti", "<cmd>MasonToolsInstall<cr>" },
+	},
+}
