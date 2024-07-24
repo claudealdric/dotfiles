8b678182140e0fcca65b77aaf7beaf84caf6afe5
claudealdric <cvaldric@gmail.com>
2024-07-24 15:36:42 -0400

    Add a note to self about the fugitive plugin

--- .config/nvim/lua/claudealdric/plugins/fugitive.lua
+++ .config/nvim/lua/claudealdric/plugins/fugitive.lua
@@ -1,3 +1,18 @@
+-- Dear future Claude,
+--
+-- You don't have much use for Fugitive because you currently mainly use git
+-- through the CLI. However, there are some useful commands that are
+-- occassionally useful. Here are some examples:
+--
+-- - `:Gsplit HEAD~3:%`: this allows you to look at the current buffer side by
+-- side with its past version.
+-- - `:Gdiffsplit`: this allows you to compare the staged version of the file
+-- with the working tree version.
+--
+-- Your future self's bestie,
+--
+-- Past Claude
+
 return {
 	"tpope/vim-fugitive",
 	event = { "CmdlineEnter" },
