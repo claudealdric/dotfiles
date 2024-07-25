ce833a98910af975d23ac195c63c2b5d9db56539
claudealdric <cvaldric@gmail.com>
2024-07-24 23:50:46 -0400

    Override the keymap for nvim-surround in visual mode
    Before, the default keymap for nvim-surround's visual mode is "S", which is
    getting overwritten by the Flash plugin. Override the default so that it
    doesn't cause any keymap collision with other plugins.

--- .config/nvim/lua/claudealdric/plugins/nvim-surround.lua
+++ .config/nvim/lua/claudealdric/plugins/nvim-surround.lua
@@ -2,5 +2,9 @@ return {
 	"kylechui/nvim-surround",
 	version = "*", -- Use for stability; omit to use `main` branch for the latest features
 	event = "CursorMoved",
-	opts = {},
+	opts = {
+		keymaps = {
+			visual = "<leader>S",
+		},
+	},
 }
