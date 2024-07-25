d24cb17c55008a9ff60cec7ea265aa322d0577ac
claudealdric <cvaldric@gmail.com>
2024-07-24 23:38:19 -0400

    Adopt the treesitter config of LazyVim

--- .config/nvim/lua/claudealdric/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/claudealdric/plugins/nvim-treesitter.lua
@@ -1,44 +1,92 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	version = false, -- last release is way too old and doesn't work on Windows
 	build = ":TSUpdate",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		local configs = require("nvim-treesitter.configs")
-
-		configs.setup({
-			auto_install = true,
-			ensure_installed = {
-				"bash",
-				"css",
-				"dockerfile",
-				"git_config",
-				"git_rebase",
-				"gitattributes",
-				"gitcommit",
-				"gitignore",
-				"go",
-				"gomod",
-				"gosum",
-				"graphql",
-				"html",
-				"javascript",
-				"json",
-				"lua",
-				"sql",
-				"toml",
-				"typescript",
-				"vim",
-				"yaml",
-			},
-			ignore_install = {
+	event = "VeryLazy",
+	-- load treesitter early when opening a file from the cmdline
+	lazy = vim.fn.argc(-1) == 0,
+	init = function(plugin)
+		-- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
+		-- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
+		-- no longer trigger the **nvim-treesitter** module to be loaded in time.
+		-- Luckily, the only things that those plugins need are the custom queries, which we make available
+		-- during startup.
+		require("lazy.core.loader").add_to_rtp(plugin)
+		require("nvim-treesitter.query_predicates")
+	end,
+	cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+	opts_extend = { "ensure_installed" },
+	opts = {
+		auto_install = true,
+		ensure_installed = {
+			"bash",
+			"css",
+			"diff",
+			"dockerfile",
+			"git_config",
+			"git_rebase",
+			"gitattributes",
+			"gitcommit",
+			"gitignore",
+			"go",
+			"gomod",
+			"gosum",
+			"graphql",
+			"html",
+			"javascript",
+			"jsdoc",
+			"json",
+			"jsonc",
+			"lua",
+			"luadoc",
+			"luap",
+			"markdown",
+			"markdown_inline",
+			"regex",
+			"sql",
+			"toml",
+			"typescript",
+			"vim",
+			"vimdoc",
+			"yaml",
+		},
+		ignore_install = {
+			"tmux",
+		},
+		highlight = {
+			enable = true,
+			disable = {
 				"tmux",
 			},
-			highlight = {
+		},
+		indent = { enable = true },
+		textobjects = {
+			move = {
 				enable = true,
-				disable = {
-					"tmux",
+				goto_next_start = {
+					["]f"] = "@function.outer",
+					["]c"] = "@class.outer",
+					["]a"] = "@parameter.inner",
+				},
+				goto_next_end = {
+					["]F"] = "@function.outer",
+					["]C"] = "@class.outer",
+					["]A"] = "@parameter.inner",
+				},
+				goto_previous_start = {
+					["[f"] = "@function.outer",
+					["[c"] = "@class.outer",
+					["[a"] = "@parameter.inner",
+				},
+				goto_previous_end = {
+					["[F"] = "@function.outer",
+					["[C"] = "@class.outer",
+					["[A"] = "@parameter.inner",
 				},
 			},
-		})
+		},
+	},
+	config = function(_, opts)
+		require("nvim-treesitter.configs").setup(opts)
 	end,
 }
