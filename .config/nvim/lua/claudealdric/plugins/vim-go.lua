5213183056aea4e46ce76497d2e1b4334318a44b
claudealdric <cvaldric@gmail.com>
2025-05-09 12:47:16 -0400

    Install vim-go plugin

---
+++ .config/nvim/lua/claudealdric/plugins/vim-go.lua
@@ -0,0 +1,11 @@
+return {
+	"fatih/vim-go",
+	event = "VeryLazy",
+	init = function()
+		vim.g.go_addtags_transform = "camelcase"
+		vim.g.go_doc_keywordprg_enabled = 0
+		vim.g.go_fmt_autosave = 0
+		vim.g.go_imports_autosave = 0
+		vim.g.go_mod_fmt_autosave = 0
+	end,
+}
