de3d27623955224d74aa2b4d756083c9ed2a0a19
claudealdric <cvaldric@gmail.com>
2025-05-15 08:25:51 -0400

    Rework format on save rules

--- .config/nvim/lua/claudealdric/plugins/conform.lua
+++ .config/nvim/lua/claudealdric/plugins/conform.lua
@@ -48,11 +48,34 @@ return {
 				-- have other formatters configured.
 				["_"] = { "trim_whitespace" },
 			},
-			format_on_save = {
-				-- These options will be passed to conform.format()
-				timeout_ms = 500,
-				lsp_format = "fallback",
-			},
+			format_on_save = function(bufnr)
+				-- Disable with a global or buffer-local variable
+				if
+					vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat
+				then
+					return
+				end
+
+				-- Get the full path and extract just the filename
+				local filename =
+					vim.fn.fnamemodify(vim.api.nvim_buf_get_name(bufnr), ":t")
+
+				-- Skip formatting for specific filenames
+				local excluded = {
+					["values.yaml"] = true,
+					["values.yml"] = true,
+					["application.json"] = true,
+				}
+
+				if excluded[filename] then
+					return
+				end
+
+				return {
+					async = true,
+					lsp_format = "fallback",
+				}
+			end,
 		})
 
 		vim.keymap.set({ "n", "v" }, "<leader>;", function()
